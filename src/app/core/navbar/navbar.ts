f84ff14cb431567483bd8853320d8f4a69710303
Ganesh <ganesh.dev.nr@gmail.com>
2026-05-13 19:57:03 +0530

    show user info in the navbar

--- src/app/core/navbar/navbar.ts
+++ src/app/core/navbar/navbar.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { selectAuthUser } from '../../store/auth/auth.selectors';
 
 @Component({
   selector: 'app-navbar',
   imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  private readonly store = inject(Store);
+
+  readonly user = this.store.selectSignal(selectAuthUser);
+}
