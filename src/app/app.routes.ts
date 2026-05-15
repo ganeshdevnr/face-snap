c2d1b16ac3b7fbae9175484568cffae350d9eab1
Ganesh <ganesh.dev.nr@gmail.com>
2026-05-15 21:07:35 +0530

    friend page created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { LoginPage } from './pages/login/login';
 import { SignUpPage } from './pages/sign-up/sign-up';
 import { FeedPage } from './pages/feed/feed';
 import { MessagesPage } from './pages/messages/messages';
+import { FriendsPage } from './pages/friends/friends';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'feed', pathMatch: 'full' },
@@ -11,4 +12,5 @@ export const routes: Routes = [
   { path: 'sign-up', component: SignUpPage },
   { path: 'feed', component: FeedPage, canActivate: [authGuard] },
   { path: 'messages', component: MessagesPage, canActivate: [authGuard] },
+  { path: 'friends', component: FriendsPage, canActivate: [authGuard] },
 ];
---
+++ src/app/pages/friends/friends.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-friends-page',
+  templateUrl: './friends.html',
+})
+export class FriendsPage {}
